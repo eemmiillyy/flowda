8dd5a0d70df11809f34614d264db0f798346e825
eemmiillyy <emily.morgan@code.berlin>
2022-06-12 10:49:11 +0200

    successful sink

--- src/main/java/pdpDataProjections/DataFlowJob.java
+++ src/main/java/pdpDataProjections/DataFlowJob.java
@@ -3,7 +3,9 @@ package pdpDataProjections;
 import org.apache.flink.api.java.utils.MultipleParameterTool;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.TableResult;
 
 
 public class DataFlowJob 
@@ -33,10 +35,10 @@ public class DataFlowJob
          * Tables are registered to the catalog. 
          * Tables can be sources or sinks.
          */
-        // Set up source
-        // System.out.println( "App running" );
-        // EnvironmentSettings env = EnvironmentSettings.newInstance().build();
-        // TableEnvironment tableEnv = TableEnvironment.create(env);
+        // Set up source table 
+         System.out.println( "App running" );
+        EnvironmentSettings env = EnvironmentSettings.newInstance().build();
+        TableEnvironment tableEnv = TableEnvironment.create(env);
         
         // tableEnv.executeSql("CREATE TABLE orders (\n" +
         //     "    order_number BIGINT,\n" +
@@ -55,7 +57,7 @@ public class DataFlowJob
         //     "    'format'    = 'debezium-json'\n" +
         //     ")");
 
-         // Historical example to group based on one tables changes over daily time x column
+         // Historical example to group based on one tables changes over daily time x column aggregate
          // order revenue per product per minute
      
         // Note that this query does not check for updates every hour, it only checks the past hours when there is an update. 
@@ -63,24 +65,40 @@ public class DataFlowJob
         // should be 2 between 12 and 1 
         //tableEnv.executeSql("SELECT SUM(quantity) as `qty`, TUMBLE_START(event_time, interval '1' hour), TUMBLE_END(event_time, interval '1' hour) FROM orders group by TUMBLE(event_time, interval '1' hour);").print();
 
-        // tableEnv.executeSql("CREATE TABLE products_on_hand (\n" +
-        // "    product_id  BIGINT,\n" +
-        // "    quantity BIGINT,\n" +
-        // "    event_time TIMESTAMP(3) METADATA FROM 'value.source.timestamp' VIRTUAL, \n" +
-        // "    WATERMARK FOR event_time AS event_time - INTERVAL '5' SECOND \n" +
-        // ") WITH (\n" +
-        // "    'connector' = 'kafka',\n" +
-        // "    'topic'     = 'dbserver1.inventory.products_on_hand',\n" +
-        // "    'properties.bootstrap.servers' = 'localhost:9093',\n" +
-        // "    'debezium-json.schema-include' = 'true', \n" +
-        // "    'properties.group.id' = '1391083',\n" +
-        // "    'scan.startup.mode' = 'earliest-offset',\n" +
-        // "    'format'    = 'debezium-json'\n" +
-        // ")");
+        // SOURCE
+        tableEnv.executeSql("CREATE TABLE products_on_hand (\n" +
+        "    product_id  BIGINT,\n" +
+        "    quantity BIGINT,\n" +
+        "    event_time TIMESTAMP(3) METADATA FROM 'value.source.timestamp' VIRTUAL, \n" +
+        "    WATERMARK FOR event_time AS event_time - INTERVAL '5' SECOND \n" +
+        ") WITH (\n" +
+        "    'connector' = 'kafka',\n" +
+        "    'topic'     = 'dbserver1.inventory.products_on_hand',\n" +
+        "    'properties.bootstrap.servers' = 'localhost:9093',\n" +
+        "    'debezium-json.schema-include' = 'true', \n" +
+        "    'properties.group.id' = '1391083',\n" +
+        "    'scan.startup.mode' = 'earliest-offset',\n" +
+        "    'format'    = 'debezium-json'\n" +
+        ")");
+        Table tbl = tableEnv.from("products_on_hand");
+        tbl.printSchema();
 
+        // AGGREGATE
+       Table aggregate = tableEnv.sqlQuery("SELECT SUM(quantity) as summedQ FROM products_on_hand;");
+       aggregate.printSchema();
 
-        // Cumulative sum example
-       // tableEnv.executeSql("SELECT SUM(quantity) as summedQ FROM products_on_hand;").print();
+       // SINK
+        tableEnv.executeSql("CREATE TABLE products_on_hand_output (\n" +
+            "summedQ BIGINT\n" +
+        ") WITH (\n" +
+            "'connector' = 'kafka',\n" +
+            "'topic' = 'dbserver1.inventory.products_on_hand_output',\n" +
+            "'properties.bootstrap.servers' = 'localhost:9093',\n" +
+            "'format' = 'debezium-json'\n" +
+        ")");
+        // add view result to topic
+        aggregate.executeInsert("products_on_hand_output");
+        
     }
 }
 
